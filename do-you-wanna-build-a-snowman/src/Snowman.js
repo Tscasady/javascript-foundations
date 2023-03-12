f940a2c74965ff1cdf05e6e4b39b10d0d3eaedf2
Thomas Casady <tscasady@gmail.com>
2023-03-12 15:38:57 -0500

    Complete human/snowman.

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,4 +1,19 @@
 class Snowman {
+  constructor(data){
+    this.carrots = data.carrots
+    this.coal = data.coal
+    this.buttons = data.buttons
+    this.snowballs = data.snowballs
+    this.magicHat = false
+  }
+
+  canWearMagicHat(){
+    return this.magicHat = this.hatConditions() 
+  }
+
+  hatConditions(){
+    return (this.coal > 1) && (this.buttons > 4) && (this.carrots > 0) && (this.snowballs > 1)
+  }
 
 }
 
