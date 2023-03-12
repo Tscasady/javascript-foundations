c646a6d810bde14ce8ce3140fa816418510200ed
Thomas Casady <tscasady@gmail.com>
2023-03-12 14:29:22 -0500

    Complete dragon.

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,20 @@
+class Dragon {
+  constructor(name, rider){
+    this.name = name
+    this.rider = rider
+    this.eaten = 0
+  }
 
+  greet(){
+    return `Hi, ${this.rider}!`
+  }
+
+  eat(){
+    this.eaten += 1
+  }
+
+  get hungry(){
+    return this.eaten < 3
+  }
+}
+module.exports = Dragon
